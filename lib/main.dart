433379f48099a702387a2d1fc4d29956455e7740
eslamhasan <eslamhasanali30@gmail.com>
2026-09-12 22:30:20 -0700

    initial project setup,setup name app and app icon

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const FruitsApp());
+}
+
+class FruitsApp extends StatelessWidget {
+  const FruitsApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
+      home: Scaffold(),
+    );
+  }
+}
