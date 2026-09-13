960bf4f126d51ebf13179ca2dfa9ff0ce7c02a06
eslamhasan <eslamhasanali30@gmail.com>
2026-09-13 01:38:30 -0700

    intial onboarding view

---
+++ lib/features/onBoarding/presentation/views/onBoarding_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class OnBoardingView extends StatelessWidget {
+  const OnBoardingView({super.key});
+
+  static const String routeName = 'onBoarding_view';
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,8 +1,24 @@
+import 'package:e_commerce_app/features/onBoarding/presentation/views/onBoarding_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody> {
+  void initstate() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 2), () {
+      Navigator.pushReplacementNamed(context, OnBoardingView.routeName);
+    });
+
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
