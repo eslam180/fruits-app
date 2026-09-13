5eecf4ef890e7370773ce85d49250775ecdb241f
eslamhasan <eslamhasanali30@gmail.com>
2026-09-13 01:12:16 -0700

    generate assets into app

---
+++ lib/core/utils/assets.dart
@@ -0,0 +1,21 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesAppIcon
+  /// assets/images/app_icon.png
+  static const String imagesAppIcon = "assets/images/app_icon.png";
+
+  /// Assets for imagesSplashimage1
+  /// assets/images/splashimage1.svg
+  static const String imagesSplashimage1 = "assets/images/splashimage1.svg";
+
+  /// Assets for imagesSplashimage2
+  /// assets/images/splashimage2.svg
+  static const String imagesSplashimage2 = "assets/images/splashimage2.svg";
+
+  /// Assets for imagesSplashimage3
+  /// assets/images/splashimage3.svg
+  static const String imagesSplashimage3 = "assets/images/splashimage3.svg";
+}
+
